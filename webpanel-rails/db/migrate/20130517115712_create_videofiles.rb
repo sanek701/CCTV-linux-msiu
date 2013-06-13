8bb6ac6c452a975f00054a84da1134fc6fea28cd
Alexander Kiseliov <aakiselev@at-consulting.ru>
2013-06-13 17:07:35 +0400

    Intro

--- webpanel-rails/db/migrate/20130517115712_create_videofiles.rb
+++ webpanel-rails/db/migrate/20130517115712_create_videofiles.rb
@@ -2,9 +2,10 @@ class CreateVideofiles < ActiveRecord::Migration
   def change
     create_table :videofiles do |t|
       t.references :camera
+      t.boolean :mp4
       t.datetime :started_at
       t.datetime :finished_at
-      t.boolean :mp4
+      t.datetime :deleted_at
     end
     add_index :videofiles, :camera_id
   end
